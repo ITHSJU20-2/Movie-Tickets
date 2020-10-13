5d781a5d7ad35b1fe150c55941fb27756dc5ee53
Tobias <twadseth@icloud.com>
2020-10-13 15:15:35 +0200

    Finished code of main page and started on booking page

--- src/main/java/movietickets/Main.java
+++ src/main/java/movietickets/Main.java
@@ -1,23 +1,20 @@
 package movietickets;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import movietickets.pages.Booking;
+import movietickets.pages.MainMenu;
 
 
 public class Main extends Application {
+
+    private static final MovieList movieList = new MovieList();
+    private static Main INSTANCE;
+
+    private MainMenu mainMenu;
+    private Booking booking;
+
+    private Stage stage;
 
     public static void main(String[] args) {
         launch(args);
@@ -25,63 +22,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        INSTANCE = this;
+        this.stage = stage;
+        // Create the movie list and the movies
+        movieList.addMovie(new Movie("Star Wars", 140, "Science Fiction", "15 år", 100, "In a galaxy far far away."));
+        movieList.addMovie(new Movie("Lord of The Rings", 140, "I don't know", "15 år", 1000, "My precious."));
 
-        MovieList movieList = new MovieList();
-        movieList.addMovie(new Movie("Star Wars", 140, "Science Fiction", "PG", 100, "In a galaxy far far away."));
-        movieList.addMovie(new Movie("Lord of The Rings", 140, "I don't know", "PG", 100, "My precious."));
+        mainMenu = new MainMenu();
+        booking = new Booking();
 
-        Label description = new Label();
-        Label length = new Label();
-        Label genre = new Label();
-        Label rating = new Label();
-        Label price = new Label();
-
-        GridPane gridPane = new GridPane();
-
-        gridPane.setMinSize(400, 200);
-
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
-
-        gridPane.setVgap(10);
-        gridPane.setHgap(5);
-
-        gridPane.setAlignment(Pos.CENTER_LEFT);
-
-        ComboBox<String> dropDown = new ComboBox<>();
-
-        movieList.getMovieList().forEach(movie -> dropDown.getItems().add(movie.getTitle()));
-
-        dropDown.setValue("Select Movie");
-
-        gridPane.add(dropDown, 0, 0);
-        gridPane.add(description, 1, 0);
-        gridPane.add(length, 1, 1);
-        gridPane.add(genre, 1, 2);
-        gridPane.add(rating, 1, 3);
-        gridPane.add(price, 0, 1, 3, 1);
-
-        Scene scene = new Scene(gridPane);
-//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
-        stage.setTitle("Java Filmer");
-        stage.setScene(scene);
+        stage.setTitle("To Be Determined");
+        stage.setScene(mainMenu.getScene());
+        stage.setResizable(false);
         stage.show();
+    }
 
-        dropDown.valueProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                Movie movie = movieList.getMovieByTitle(newValue);
-                if (movie == null) {
-                    return;
-                }
-                description.setText(movie.getDescription());
-                length.setText(movie.getLength() + "");
-                genre.setText(movie.getGenre());
-                rating.setText(movie.getRating());
-                price.setText(movie.getPrice() + "");
-            }
-        });
+    public MovieList getMovieList() {
+        return movieList;
+    }
 
+    public static Main getInstance() {
+        return INSTANCE;
+    }
+
+    public MainMenu getMainMenu() {
+        return mainMenu;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public Stage getStage() {
+        return stage;
     }
 }
